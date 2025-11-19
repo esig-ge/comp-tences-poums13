f2d6976aa965df0d03974ad54a9427b3aa38ae90
poums13 <poumin.mll@eduge.ch>
2025-11-19 16:41:58 +0100

    render set up

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p#o7n0e4ery5ydoq9&vky6bhf5rnttqkh1$!-@sr&bkud)$ws&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
